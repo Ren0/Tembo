14d09d8ef45702a0a6cf1d3f3ba2bf864be5a18c
Renaud Caron <renaud.caron@newedge.com>
2016-01-07 18:32:01 +0000

    poc

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ var morgan      = require('morgan');
 var mongoose    = require('mongoose');
 
 var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
-var config = require('./config'); // get our config file
+//var config = require('./config'); // get our config file
 var User   = require('./app/models/user'); // get our mongoose model
 
 // =================================================================
 // configuration ===================================================
 // =================================================================
 var port = process.env.PORT || 8080; // used to create, sign, and verify tokens
-mongoose.connect(config.database); // connect to database
-app.set('superSecret', config.secret); // secret variable
+mongoose.connect('mongodb://localhost:27017/tembo'); // connect to database
+app.set('superSecret', 'thisisubersecret'); // secret variable
 
 // use body parser so we can get info from POST and/or URL parameters
 app.use(bodyParser.urlencoded({ extended: false }));
